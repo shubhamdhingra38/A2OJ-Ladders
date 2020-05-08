f6a438c3ead8360900732e97933a720deb0be506
Shubham Dhingra <dhingra.shubham38@gmail.com>
2020-05-09 05:27:43 +0530

    Deleted outside files

--- hq9.py
+++
@@ -1,8 +0,0 @@
-s = input()
-flg = False
-for ch in s:
-    if ch in ['H', 'Q', '9']:
-        flg = True
-        break
-if flg: print("YES")
-else: print("NO")
--- ultra.py
+++
@@ -1,9 +0,0 @@
-a = input()
-b = input()
-c = ''
-for i in range(len(a)):
-    if a[i]==b[i]:
-        c += '0'
-    else:
-        c += '1'
-print(c)
